ec55439642f37d8377a4ac0ffbd9bd033afc7a11
tangym27 <tangym27@gmail.com>
2020-08-12 18:02:57 -0400

    this whole process was a thats rough buddy initial layout

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,25 +1,62 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import { createUseStyles } from "react-jss";
+import avatar from "./avatar.png";
+import Button  from "./Button";
 
+const useStyles = createUseStyles({
+  App:{
+    backgroundImage: `url(${avatar})`,
+    height: "90vh",
+    backgroundRepeat:"no-repeat",
+    backgroundSize:"cover",
+    fontFamily:"Courier New",
+    display:"flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  header:{
+    fontSize:"2rem",
+    fontFamily:"Courier New",
+    display:"flex",
+    flexDirection: "column",
+    alignItems: "left",
+    justifyContent: "center",
+    backgroundColor:"black",
+    color: "#cb3f34",
+    height:"5vh",
+  },
+  footer:{
+    fontFamily:"Courier New",
+    height:"5vh",
+    backgroundColor:"black",
+    textAlign:"center",
+    color: "#d0732f",
+  },
+  a:{
+    color: "white",
+    "&:hover":{
+      color:"red",
+    },
+  },
+  p:{
+    fontSize: "3rem",
+    color:"black",
+  },
+});
 function App() {
+  const classes= useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className={classes.header}> Cards Against the Fire Nation
       </header>
+      <div className={classes.App}>
+      <p className={classes.p}> Welcome! </p>
+      <Button name="Generate Cards">  </Button>
     </div>
+    <footer className={classes.footer}> A <a className={classes.a} href="https://spark.torchnyu.com/" target="blank"> Spark</a>y Sparky Boom Boom Project </footer>
+  </div>
   );
 }
 
