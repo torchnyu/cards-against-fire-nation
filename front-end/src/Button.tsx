75c5d80e72dfbd48a15438c5b91fa827657e413d
tangym27 <tangym27@gmail.com>
2020-08-12 17:28:42 -0400

    button

---
+++ front-end/src/Button.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { createUseStyles } from "react-jss";
+
+interface Props {
+  name: string
+}
+
+const useStyles = createUseStyles({
+  button:{
+    fontSize:"2rem",
+    fontFamily:"Courier New",
+
+    backgroundColor:"#969595",
+    color: "black",
+    borderRadius: "7px",
+    "&:hover":{
+        filter: "brightness(75%)",
+    },
+  },
+});
+
+const Button: React.FC<Props> = ({ name }) => {
+  const classes= useStyles();
+
+  return <button className={classes.button}>
+    {name}
+  </button>
+}
+
+export default Button;
