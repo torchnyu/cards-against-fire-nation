a7aa03410a4d30198019c119405a0a846461e0fa
Henry Liu <hliu01@stuy.edu>
2020-08-19 18:50:00 -0400

    made some changes

--- backend/parse.py
+++ backend/parse.py
@@ -1,5 +1,10 @@
 from bs4 import BeautifulSoup
+import re
 counter = 0
+dialogue_matcher = re.compile("^[a-zA-Z0-9]+: .+$")
+parenthesis_picker = re.compile("\\([^()]*\\)")
+name_remover = re.compile("^[a-zA-Z0-9]+: +")
+
 
 
 def parse():
@@ -13,4 +18,38 @@ def parse():
             f.write(text)
         counter += 1
 
-parse()
+def lines():
+    with open(f"avatar{counter}","w") as f:
+        f.write(text)
+
+
+
+
+
+
+    with open(f"transcripts/avatar{1}") as f:
+        data = f.read()
+    data = data.split("\n")
+    counter = 0
+    for y in data:
+        if y == "":
+            break
+        counter += 1
+    data2 = []
+    data3 = []
+    data4 = []
+    for x in data[counter:]:
+        if dialogue_matcher.match(x):
+            data2.append(x)
+    for thing in data2:
+        data3.append(parenthesis_picker.sub("",thing))
+    for thing in data3:
+        data4.append(name_remover.sub("",thing))
+    print (data4)
+
+
+
+
+
+
+lines()
